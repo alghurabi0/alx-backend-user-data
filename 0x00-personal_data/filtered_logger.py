4ca9a2c4136a671da1dda0752d6521c873773d04
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-04-10 09:10:22 -0400

    format

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """obfuscating sensitive data"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -15,3 +16,25 @@ def filter_datum(
         message = re.sub(f'{field}=(.*?){separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """instance init"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter piece of data"""
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            super().format(record),
+            self.SEPARATOR
+            )

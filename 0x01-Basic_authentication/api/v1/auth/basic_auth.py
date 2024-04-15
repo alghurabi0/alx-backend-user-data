3f1081944b946b4975427787af048dd128c2ba5d
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-04-15 18:28:24 -0400

    basic auth auth header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """basic auth class"""
+from werkzeug.datastructures import Authorization
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """basic auth class"""
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:  # noqa
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        prefix = 'Basic '
+        if not authorization_header.startswith(prefix):
+            return None
+        value = authorization_header[len(prefix):].strip()
+        return value

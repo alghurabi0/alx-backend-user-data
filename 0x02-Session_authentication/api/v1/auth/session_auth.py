ba81eb474d1ca88955091e8828c3027d84932b0c
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-04-19 04:01:12 +0000

    self

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -24,4 +24,4 @@ class SessionAuth(Auth):
             return None
         if not isinstance(session_id, str):
             return None
-        return user_id_by_session_id.get(session_id)
+        return self.user_id_by_session_id.get(session_id)
